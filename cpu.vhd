c5dc269d1ddb3a3cea56de4a99b90a98fe5551d7
karim taha <krm.taha@outlook.com>
2022-01-08 20:30:07 +0200

    fixed some indentation

--- cpu.vhd
+++ cpu.vhd
@@ -12,24 +12,24 @@ END cpu;
 ARCHITECTURE cpu OF cpu IS
 	--pretty much all the signals we'll need in the processor
 	SIGNAL not_clk : STD_LOGIC;
-	SIGNAL inst_memo : STD_LOGIC_VECTOR(31 DOWNTO 0);       --input from instruction memory (temporary)
-	SIGNAL stage1_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  --f/d
-	SIGNAL stage2_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  --d/e
-	SIGNAL stage3_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  --e/m
-	SIGNAL stage4_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  --m/wb
-	SIGNAL alu_operand1 : STD_LOGIC_VECTOR(15 DOWNTO 0);	  --operand for alu
-	SIGNAL alu_operand2 : STD_LOGIC_VECTOR(15 DOWNTO 0);	  --operand for alu
-	SIGNAL wb_data : STD_LOGIC_VECTOR(15 DOWNTO 0);			  --data for write back
-	SIGNAL memo_addr : STD_LOGIC_VECTOR(31 DOWNTO 0);		  --address for data memory
-	SIGNAL stack_addr : STD_LOGIC_VECTOR(31 DOWNTO 0);		  --address for stack
-	SIGNAL CS : STD_LOGIC_VECTOR(27 DOWNTO 0);				  --control signals
-	SIGNAL read_data1 : STD_LOGIC_VECTOR(15 DOWNTO 0);		  --data for rsrc1
-	SIGNAL read_data2 : STD_LOGIC_VECTOR(15 DOWNTO 0);		  --data for rsrc2
-	SIGNAL alu_result : STD_LOGIC_VECTOR(15 DOWNTO 0);		  --result of alu
-	SIGNAL memo_data16 : STD_LOGIC_VECTOR(15 DOWNTO 0);	  --data 16 form data memory
-	SIGNAL memo_data32 : STD_LOGIC_VECTOR(31 DOWNTO 0);	  --data 32 form data memory
-	SIGNAL index : STD_LOGIC_VECTOR(1 DOWNTO 0);				  --index
-    SIGNAL exec_result:  std_logic_vector(15 DOWNTO 0); -- the output of the final mux in the execution stage
+	SIGNAL inst_memo : STD_LOGIC_VECTOR(31 DOWNTO 0);       	--input from instruction memory (temporary)
+	SIGNAL stage1_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  		--f/d
+	SIGNAL stage2_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  		--d/e
+	SIGNAL stage3_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  		--e/m
+	SIGNAL stage4_reg : STD_LOGIC_VECTOR(127 DOWNTO 0);	  		--m/wb
+	SIGNAL alu_operand1 : STD_LOGIC_VECTOR(15 DOWNTO 0);	  	--operand for alu
+	SIGNAL alu_operand2 : STD_LOGIC_VECTOR(15 DOWNTO 0);	  	--operand for alu
+	SIGNAL wb_data : STD_LOGIC_VECTOR(15 DOWNTO 0);			  	--data for write back
+	SIGNAL memo_addr : STD_LOGIC_VECTOR(31 DOWNTO 0);		  	--address for data memory
+	SIGNAL stack_addr : STD_LOGIC_VECTOR(31 DOWNTO 0);		  	--address for stack
+	SIGNAL CS : STD_LOGIC_VECTOR(27 DOWNTO 0);				  	--control signals
+	SIGNAL read_data1 : STD_LOGIC_VECTOR(15 DOWNTO 0);		  	--data for rsrc1
+	SIGNAL read_data2 : STD_LOGIC_VECTOR(15 DOWNTO 0);		  	--data for rsrc2
+	SIGNAL alu_result : STD_LOGIC_VECTOR(15 DOWNTO 0);		  	--result of alu
+	SIGNAL memo_data16 : STD_LOGIC_VECTOR(15 DOWNTO 0);	  		--data 16 form data memory
+	SIGNAL memo_data32 : STD_LOGIC_VECTOR(31 DOWNTO 0);	  		--data 32 form data memory
+	SIGNAL index : STD_LOGIC_VECTOR(1 DOWNTO 0);				--index
+    SIGNAL exec_result:  std_logic_vector(15 DOWNTO 0); 		-- the output of the final mux in the execution stage
 	
 	-- flag register signals:
 	signal flag_in : std_logic_vector(3 downto 0);
@@ -82,7 +82,7 @@ BEGIN
 		stage1_reg(9 DOWNTO 7), 										--outRdst_index
 		stage1_reg(12 DOWNTO 10), 										--outRsrc1_index
 		stage1_reg(15 DOWNTO 13), 										--outRsrc2_index
-		index, 																--outInt_index
+		index, 															--outInt_index
 		stage1_reg(63 DOWNTO 32)										--outPC
 		);
 
@@ -154,7 +154,7 @@ BEGIN
 			not_clk,
 			rst,
 			stage1_reg(4 DOWNTO 0), --opcode
-			freeze --freeze
+			freeze 					--freeze
 			);
 
 	--decode-execute-buffer
@@ -164,34 +164,34 @@ BEGIN
 		'1',
 		CS,
 		"000000000", 					--inG
-		stage1_reg(9 DOWNTO 7), 	--inRdst_index
+		stage1_reg(9 DOWNTO 7), 		--inRdst_index
 		read_data1,						--inRsrc_data1
 		read_data2, 					--inRsrc_data2
-		stage1_reg(31 DOWNTO 16), 	--inImm
+		stage1_reg(31 DOWNTO 16), 		--inImm
 		index, 							--inInt_index
-		stage1_reg(12 DOWNTO 10), 	--inRsrc1_index
-		stage1_reg(15 DOWNTO 13), 	--inRsrc2_index
-		stage1_reg(63 DOWNTO 32), 	--inPC
-		stage2_reg(27 DOWNTO 0), 	--outCS
+		stage1_reg(12 DOWNTO 10), 		--inRsrc1_index
+		stage1_reg(15 DOWNTO 13), 		--inRsrc2_index
+		stage1_reg(63 DOWNTO 32), 		--inPC
+		stage2_reg(27 DOWNTO 0), 		--outCS
 		OPEN, 							--outG
-		stage2_reg(39 DOWNTO 37), 	--outRdst_index
-		stage2_reg(55 DOWNTO 40), 	--outRsrc_data1
-		stage2_reg(71 DOWNTO 56), 	--outRsrc_data2
-		stage2_reg(87 DOWNTO 72), 	--outImm
-		stage2_reg(89 DOWNTO 88), 	--outInt_index
-		stage2_reg(92 DOWNTO 90), 	--outRsrc1_index
-		stage2_reg(95 DOWNTO 93), 	--outRsrc2_index
-		stage2_reg(127 DOWNTO 96)	--outPC
+		stage2_reg(39 DOWNTO 37), 		--outRdst_index
+		stage2_reg(55 DOWNTO 40), 		--outRsrc_data1
+		stage2_reg(71 DOWNTO 56), 		--outRsrc_data2
+		stage2_reg(87 DOWNTO 72), 		--outImm
+		stage2_reg(89 DOWNTO 88), 		--outInt_index
+		stage2_reg(92 DOWNTO 90), 		--outRsrc1_index
+		stage2_reg(95 DOWNTO 93), 		--outRsrc2_index
+		stage2_reg(127 DOWNTO 96)		--outPC
 		);
 
 	--execute-logic
 	--mux1(operand1)
 	WITH stage2_reg(0) SELECT
 	alu_operand1 <=
-		stage2_reg(55 DOWNTO 40) WHEN '0', --rsrc1(16)
-		stage2_reg(87 DOWNTO 72) WHEN OTHERS;--imm(16)
+		stage2_reg(55 DOWNTO 40) WHEN '0', 		--rsrc1(16)
+		stage2_reg(87 DOWNTO 72) WHEN OTHERS;	--imm(16)
 	--mux2(operand1)
-	alu_operand2 <= stage2_reg(71 DOWNTO 56);--rsrc2(16)
+	alu_operand2 <= stage2_reg(71 DOWNTO 56);	--rsrc2(16)
 	--alu module
 	alu : ENTITY work.alu PORT MAP(not_clk, alu_operand1, alu_operand2, stage2_reg(4 DOWNTO 2), alu_result, flag_in_alu);
 	flag: entity work.flag_reg port map (clk, rst, not CS(12), flag_in, flag_out);		--CS(12) is SaveRef (not enable)
@@ -230,22 +230,22 @@ BEGIN
 		clk,
 		rst,
 		'1',
-		cs_2_3, 	--inCS
-		stage2_reg(36 DOWNTO 28), 	--inG1
-		stage2_reg(39 DOWNTO 37), 	--inRdst_index
-		stage2_reg(55 DOWNTO 40), 	--inRsrc_data1
+		cs_2_3, 						--inCS
+		stage2_reg(36 DOWNTO 28), 		--inG1
+		stage2_reg(39 DOWNTO 37), 		--inRdst_index
+		stage2_reg(55 DOWNTO 40), 		--inRsrc_data1
 		x"0000", 						--inG2
 		exec_result, 					--inResult
-		stage2_reg(89 DOWNTO 88), 	--inInt_index
-		stage2_reg(92 DOWNTO 90), 	--inRsrc1_index
-		stage2_reg(95 DOWNTO 93), 	--inRsrc2_index
-		stage2_reg(127 DOWNTO 96),	--inPC
-		stage3_reg(27 DOWNTO 0), 	--outCS
-		stage3_reg(55 DOWNTO 40), 	--outRsrc_data1
-		stage3_reg(87 DOWNTO 72), 	--outResult
-		stage3_reg(39 DOWNTO 37), 	--outRdst_index
-		stage3_reg(89 DOWNTO 88), 	--outInt_index
-		stage3_reg(127 DOWNTO 96)	--outPC
+		stage2_reg(89 DOWNTO 88), 		--inInt_index
+		stage2_reg(92 DOWNTO 90), 		--inRsrc1_index
+		stage2_reg(95 DOWNTO 93), 		--inRsrc2_index
+		stage2_reg(127 DOWNTO 96),		--inPC
+		stage3_reg(27 DOWNTO 0), 		--outCS
+		stage3_reg(55 DOWNTO 40), 		--outRsrc_data1
+		stage3_reg(87 DOWNTO 72), 		--outResult
+		stage3_reg(39 DOWNTO 37), 		--outRdst_index
+		stage3_reg(89 DOWNTO 88), 		--outInt_index
+		stage3_reg(127 DOWNTO 96)		--outPC
 		);
 
 	--memory logic
@@ -255,8 +255,8 @@ BEGIN
 	WITH stage3_reg(6 DOWNTO 5) SELECT
 	memo_addr <=
 		(x"0000") & stage3_reg(87 DOWNTO 72) WHEN "00", --result of alu
-		stack_addr WHEN "01", --stack_addr
-		(OTHERS => '0') WHEN OTHERS;--temp
+		stack_addr WHEN "01", 							--stack_addr
+		(OTHERS => '0') WHEN OTHERS;					--temp
 	--memory module
 	data_memory : ENTITY work.data_memory PORT MAP(not_clk, stage3_reg(7), memo_addr, '0', stage3_reg(55 DOWNTO 40), stage3_reg(127 DOWNTO 96), memo_data16, memo_data32);
 
@@ -265,21 +265,21 @@ BEGIN
 		clk,
 		rst,
 		'1',
-		stage3_reg(27 DOWNTO 0), 	--inCS
-		stage3_reg(36 DOWNTO 28), 	--inG
-		stage3_reg(39 DOWNTO 37), 	--inRdst_index
-		stage3_reg(55 DOWNTO 40), 	--inRsrc_data1
+		stage3_reg(27 DOWNTO 0), 		--inCS
+		stage3_reg(36 DOWNTO 28), 		--inG
+		stage3_reg(39 DOWNTO 37), 		--inRdst_index
+		stage3_reg(55 DOWNTO 40), 		--inRsrc_data1
 		memo_data16, 					--inData16
-		stage3_reg(87 DOWNTO 72), 	--inResult
+		stage3_reg(87 DOWNTO 72), 		--inResult
 		memo_data32, 					--inData32
 		"00", 							--inG2
 		"000", 							--inG3
 		"000", 							--inG4
-		stage4_reg(27 DOWNTO 0), 	--outCS
-		stage4_reg(39 DOWNTO 37), 	--outRdst_index
-		stage4_reg(87 DOWNTO 72), 	--outResult
-		stage4_reg(127 DOWNTO 96), --outData32
-		stage4_reg(71 DOWNTO 56)	--outData16
+		stage4_reg(27 DOWNTO 0), 		--outCS
+		stage4_reg(39 DOWNTO 37), 		--outRdst_index
+		stage4_reg(87 DOWNTO 72), 		--outResult
+		stage4_reg(127 DOWNTO 96), 		--outData32
+		stage4_reg(71 DOWNTO 56)		--outData16
 		);
 
 	--writeback logic
