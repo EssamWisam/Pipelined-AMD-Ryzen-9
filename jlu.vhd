138c8f3f27264954e5d42c9a29c13f442423544f
Muhammad-saad-2000 <mohamedmiogh2@gmail.com>
2022-01-08 17:19:44 +0200

    small modification on jlu.

--- jlu.vhd
+++ jlu.vhd
@@ -19,23 +19,25 @@ begin
 	--logic
 	process (clk)
 	begin
-		if rising_edge(clk) and pcc='1' then
-			if jm = "000" and flag_out(3)='1' then			      	-- Check if zero
-				Cond_reg <= '1';
-            flag_in(3) <= '0';
-			elsif jm = "001" and flag_out(2)='1' then					-- Check if negative
-				Cond_reg <= '1';
-            flag_in(2) <= '0';
-			elsif jm = "010" and flag_out(1 downto 0)="01" then	-- Check if carry
-				Cond_reg <= '1';
-            flag_in(1 downto 0) <= "00";
-			elsif jm = "011" then
-				Cond_reg <= '1';
+		if rising_edge(clk) then 
+			if pcc='1' then
+				if jm = "000" and flag_out(3)='1' then			      	-- Check if zero
+					Cond_reg <= '1';
+				flag_in(3) <= '0';
+				elsif jm = "001" and flag_out(2)='1' then					-- Check if negative
+					Cond_reg <= '1';
+				flag_in(2) <= '0';
+				elsif jm = "010" and flag_out(1 downto 0)="01" then	-- Check if carry
+					Cond_reg <= '1';
+				flag_in(1 downto 0) <= "00";
+				elsif jm = "011" then
+					Cond_reg <= '1';
+				else
+					Cond_reg <= '0';										 		-- Still haven't considered other PC changers
+				end if;
 			else
-				Cond_reg <= '0';										 		-- Still haven't considered other PC changers
+				Cond_reg <='0';														-- Cond = 0 for non PC changers.
 			end if;
-		else
-			Cond_reg <='0';														-- Cond = 0 for non PC changers.
 		end if;
 	end process;
 	--out
