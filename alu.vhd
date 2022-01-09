b177a95665f6ead784dc2213a7163943392613d3
Ahmedmma72 <ahmedmma561@gmail.com>
2022-01-09 15:39:32 +0200

    flag fixed

--- alu.vhd
+++ alu.vhd
@@ -12,43 +12,49 @@ entity alu is
 	);
 end alu;
 architecture alu of alu is
-	signal result_reg : std_logic_vector(15 downto 0);
+	signal result_regz : std_logic_vector(15 downto 0);
+	signal flag_regz :  std_logic_vector(3 downto 0);
 begin
 	--logic
 	process (clk)
+	variable result_reg : std_logic_vector(15 downto 0);
+	variable flag_reg :  std_logic_vector(3 downto 0);
 	begin
 		if rising_edge(clk) then
 			if op = "000" then			-- forward upper
-				result_reg <= a;
+				result_reg := a;
 			elsif op = "001" then		-- forward loer
-				result_reg <= b;
+				result_reg := b;
 			elsif op = "010" then		-- and
-				result_reg <= a and b;
-				flag_in(3) <= '1' when result = x"0000" else '0';					--zero flag
-				flag_in(2) <= result(15); 													--sign flag
+				result_reg := a and b;
+				flag_reg(3) := '1' when result_reg = x"0000" else '0';					--zero flag
+				flag_reg(2) := result_reg(15); 													--sign flag
 			elsif op = "011" then		-- add
-				(flag_in(1 downto 0), result_reg) <= std_logic_vector(unsigned("00" & a) + unsigned("00" & b));
-				flag_in(3) <= '1' when result = x"0000" else '0';					
-				flag_in(2) <= result(15); 													
+				(flag_reg(1 downto 0), result_reg) := std_logic_vector(unsigned("00" & a) + unsigned("00" & b));
+				flag_reg(3) := '1' when result_reg = x"0000" else '0';					
+				flag_reg(2) := result_reg(15); 													
 			elsif op = "100" then		-- subtract
-				(flag_in(1 downto 0), result_reg) <= std_logic_vector(unsigned("00" & a) - unsigned("00" & b));
-				flag_in(3) <= '1' when result = x"0000" else '0';					
-				flag_in(2) <= result(15); 													
+				(flag_reg(1 downto 0), result_reg) := std_logic_vector(unsigned("00" & a) - unsigned("00" & b));
+				flag_reg(3) := '1' when result_reg = x"0000" else '0';					
+				flag_reg(2) := result_reg(15); 													
 			elsif op = "101" then		-- increment
-				result_reg <= std_logic_vector(unsigned(a) + 1);
-				flag_in(3) <= '1' when result = x"0000" else '0';					
-				flag_in(2) <= result(15);
+				result_reg := std_logic_vector(unsigned(a) + 1);
+				flag_reg(3) := '1' when result_reg = x"0000" else '0';					
+				flag_reg(2) := result_reg(15);
 			elsif  op = "110" then		--setc
-				flag_in(1 downto 0) <= "01";
+				flag_reg(1 downto 0) := "01";
 			elsif op = "111" then		--not
-				result_reg <= not a;
-				flag_in(3) <= '1' when result = x"0000" else '0';					
-				flag_in(2) <= result(15); 													
+				result_reg := not a;
+				flag_reg(3) := '1' when result_reg = x"0000" else '0';					
+				flag_reg(2) := result_reg(15); 													
 			end if;
 		end if;
+		result_regz <= result_reg;
+		flag_regz <= flag_reg;
 	end process;
 	--out
-	result <= result_reg;
+	result <= result_regz;
+	flag_in <= flag_regz;
 
 
 end alu;
